056170057aee284ad39a0615db8680f32907bcb7
Daniel Borre <danisecundarioxd@gmail.com>
2025-11-30 00:47:33 -0500

    Smear cursor windows

--- lua/plugins/smear-cursor.lua
+++ lua/plugins/smear-cursor.lua
@@ -2,12 +2,10 @@ local platform = require "utils.platform"
 
 -- smear-cursor may have issues on Windows terminals without proper support
 -- Disable on Windows by default, users can re-enable if their terminal supports it
-local enabled = not platform.is_windows
 
 return {
   "sphamba/smear-cursor.nvim",
   lazy = false,
-  cond = enabled,
   opts = {
     stiffness = 0.8,
     trailing_stiffness = 0.5,
